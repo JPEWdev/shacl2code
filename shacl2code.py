c50192d74658cd36f8cb30ab8f99b147cc80dc78
Joshua Watt <JPEWhacker@gmail.com>
2024-01-10 09:26:29 -0700

    shacl2code: Split out model to a class
    Splits out the model data into a class

--- shacl2code.py
+++ shacl2code.py
@@ -46,82 +46,106 @@ def get_langs():
     return langs
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Convert JSON-LD model to python")
-    parser.add_argument(
-        "--lang",
-        help="Output Language",
-        choices=get_langs(),
-        required=True,
-    )
-    parser.add_argument("input", help="Input JSON-LD model")
-    parser.add_argument("output", type=Path, help="Output file")
+class Model(object):
+    def __init__(self, model_data):
+        self.model = jsonld.expand(model_data)
+        self.context = model_data.get("@context", {})
+        self.compact = {}
+        self.objects = {}
+        self.classes = []
+        self.enums = []
 
-    args = parser.parse_args()
+        for obj in self.model:
+            self.compact[obj["@id"]] = jsonld.compact(obj, self.context)
+            del self.compact[obj["@id"]]["@context"]
 
-    if "://" in args.input:
-        with urllib.request.urlopen(args.input) as url:
-            model = json.load(url)
-    else:
-        with Path(args.input).open("r") as f:
-            model = json.load(f)
+            self.objects[obj["@id"]] = obj
 
-    context = model.get("@context", {})
+            if "http://www.w3.org/2002/07/owl#oneOf" in obj:
+                self.enums.append(obj)
+            elif "@type" in obj:
+                if "http://www.w3.org/2002/07/owl#Class" in obj["@type"]:
+                    self.classes.append(obj)
 
-    model = jsonld.expand(model)
-    compact = {}
+        self.classes.sort(key=lambda c: c["@id"])
+        self.class_ids = set(c["@id"] for c in self.classes)
 
-    objects = {}
-    classes = []
-    enums = []
+        self.enums.sort(key=lambda e: e["@id"])
+        self.enum_ids = set(e["@id"] for e in self.enums)
 
-    for obj in model:
-        compact[obj["@id"]] = jsonld.compact(obj, context)
-        del compact[obj["@id"]]["@context"]
+    def is_enum(self, _id):
+        return _id in self.enum_ids
 
-        objects[obj["@id"]] = obj
+    def is_class(self, _id):
+        return _id in self.class_ids
 
-        if "http://www.w3.org/2002/07/owl#oneOf" in obj:
-            enums.append(obj)
-        elif "@type" in obj:
-            if "http://www.w3.org/2002/07/owl#Class" in obj["@type"]:
-                classes.append(obj)
-
-    classes.sort(key=lambda c: c["@id"])
-    class_ids = set(c["@id"] for c in classes)
-
-    enums.sort(key=lambda e: e["@id"])
-    enum_ids = set(e["@id"] for e in enums)
-
-    def get_compact(obj, *path):
+    def get_compact(self, obj, *path):
         """
         Returns the "compacted" name of an object, that is the name of the
         object with the context applied
         """
-        nonlocal compact
-        v = compact[obj["@id"]]
+        v = self.compact[obj["@id"]]
         for p in path:
             v = v[p]
         return v
 
-    def get_class_name(c):
-        return get_compact(c, "@id").replace(":", "_")
+    def get_class_name(self, c):
+        """
+        Returns the name for a class that should be used in Code
+        """
+        return self.get_compact(c, "@id").replace(":", "_")
 
-    def get_comment(obj, indent=0):
+    def get_comment(self, obj, indent=0):
+        """
+        Get the comment for a object, or "" if the object has no comment
+        """
         comment = get_prop(
-            obj, "http://www.w3.org/2000/01/rdf-schema#comment", "@value"
+            obj,
+            "http://www.w3.org/2000/01/rdf-schema#comment",
+            "@value",
         )
         if not comment:
             return ""
 
         return comment
 
-    def get_class_properties(cls):
-        nonlocal objects
+    def get_template_enums(self):
+        """
+        Returns a list of enums suitable for passing to a Jinja template
+        """
+        template_enums = []
+        for e in self.enums:
+            values = []
+            for v in get_prop(e, "http://www.w3.org/2002/07/owl#oneOf", "@list"):
+                values.append(
+                    {
+                        "id": v["@id"],
+                        "varname": to_var_name(v["@id"].split("/")[-1]),
+                        "comment": self.get_comment(self.objects[v["@id"]]),
+                    }
+                )
+            values.sort(key=lambda v: v["id"])
+
+            template_enums.append(
+                {
+                    "id": e["@id"],
+                    "clsname": self.get_class_name(e),
+                    "enum_values": values,
+                    "comment": self.get_comment(e),
+                }
+            )
+        template_enums.sort(key=lambda e: e["id"])
+        return template_enums
+
+    def get_class_template_properties(self, cls):
+        """
+        Returns a list of properties for a class suitable for passing to a
+        Jinja template
+        """
 
         props = []
         for prop_id in cls.get("http://www.w3.org/ns/shacl#property", []):
-            prop = objects[prop_id["@id"]]
+            prop = self.objects[prop_id["@id"]]
             name = prop["http://www.w3.org/ns/shacl#name"][0]["@value"]
             prop_path = get_prop(
                 prop,
@@ -132,7 +156,7 @@ def main():
             p = {
                 "varname": to_var_name(name),
                 "path": prop_path,
-                "comment": get_comment(objects[prop_path]),
+                "comment": self.get_comment(self.objects[prop_path]),
                 "max_count": get_prop(
                     prop,
                     "http://www.w3.org/ns/shacl#maxCount",
@@ -147,12 +171,14 @@ def main():
                 ),
             }
 
-            prop_cls = get_prop(prop, "http://www.w3.org/ns/shacl#class", "@id")
-            if prop_cls:
-                if prop_cls in enum_ids:
-                    p["enumname"] = get_class_name(objects[prop_cls])
+            prop_cls_id = get_prop(prop, "http://www.w3.org/ns/shacl#class", "@id")
+            if prop_cls_id:
+                if self.is_enum(prop_cls_id):
+                    p["enumname"] = self.get_class_name(self.objects[prop_cls_id])
+                elif self.is_class(prop_cls_id):
+                    p["clsname"] = self.get_class_name(self.objects[prop_cls_id])
                 else:
-                    p["clsname"] = get_class_name(objects[prop_cls])
+                    raise Exception(f"Unknown type '{prop_cls_id}'")
             else:
                 p["datatype"] = get_prop(
                     prop,
@@ -164,73 +190,79 @@ def main():
 
         return props
 
-    template_enums = []
-    for e in enums:
-        values = []
-        for v in get_prop(e, "http://www.w3.org/2002/07/owl#oneOf", "@list"):
-            values.append(
+    def get_template_classes(self):
+        done_classes = set()
+        classes = self.classes[:]
+
+        template_classes = []
+        while classes:
+            c = classes.pop(0)
+
+            parents = []
+            for p in c.get("http://www.w3.org/2000/01/rdf-schema#subClassOf", []):
+                if p["@id"] in self.objects:
+                    parents.append(self.get_class_name(self.objects[p["@id"]]))
+
+            # If any parent classes of this class are outstanding, then push it
+            # back on the end of the class list and try again. This ensures that
+            # derived classes are always written after any parent classes
+            if not all(p in done_classes for p in parents):
+                classes.append(c)
+                continue
+
+            clsname = self.get_class_name(c)
+            template_classes.append(
                 {
-                    "id": v["@id"],
-                    "varname": to_var_name(v["@id"].split("/")[-1]),
-                    "comment": get_comment(objects[v["@id"]]),
+                    "id": c["@id"],
+                    "clsname": clsname,
+                    "parents": parents,
+                    "comment": self.get_comment(c),
+                    "props": self.get_class_template_properties(c),
                 }
             )
-        values.sort(key=lambda v: v["id"])
+            done_classes.add(clsname)
 
-        template_enums.append(
-            {
-                "id": e["@id"],
-                "clsname": get_class_name(e),
-                "enum_values": values,
-                "comment": get_comment(e),
-            }
+        return template_classes
+
+    def render_template(self, lang):
+        def abort_helper(msg):
+            raise TemplateRuntimeError(msg)
+
+        env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+        env.globals["abort"] = abort_helper
+        template = env.get_template(lang + ".j2")
+
+        return template.render(
+            disclaimer=f"This file was automatically generated by {THIS_FILE.name}. DO NOT MANUALLY MODIFY IT",
+            enums=self.get_template_enums(),
+            classes=self.get_template_classes(),
         )
-    template_enums.sort(key=lambda e: e["id"])
 
-    done_classes = set()
-    template_classes = []
-    while classes:
-        c = classes.pop(0)
 
-        parents = []
-        for p in c.get("http://www.w3.org/2000/01/rdf-schema#subClassOf", []):
-            if p["@id"] in objects:
-                parents.append(get_class_name(objects[p["@id"]]))
+def main():
+    parser = argparse.ArgumentParser(description="Convert JSON-LD model to python")
+    parser.add_argument(
+        "--lang",
+        help="Output Language",
+        choices=get_langs(),
+        required=True,
+    )
+    parser.add_argument("input", help="Input JSON-LD model (path or URL)")
+    parser.add_argument("output", type=Path, help="Output file")
 
-        # If any parent classes of this class are outstanding, then push it
-        # back on the end of the class list and try again. This ensures that
-        # derived classes are always written after any parent classes
-        if not all(p in done_classes for p in parents):
-            classes.append(c)
-            continue
+    args = parser.parse_args()
 
-        clsname = get_class_name(c)
-        template_classes.append(
-            {
-                "id": c["@id"],
-                "clsname": clsname,
-                "parents": parents,
-                "comment": get_comment(c),
-                "props": get_class_properties(c),
-            }
-        )
-        done_classes.add(clsname)
+    if "://" in args.input:
+        with urllib.request.urlopen(args.input) as url:
+            model_data = json.load(url)
+    else:
+        with Path(args.input).open("r") as f:
+            model_data = json.load(f)
 
-    def abort_helper(msg):
-        raise TemplateRuntimeError(msg)
-
-    env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
-    env.globals["abort"] = abort_helper
-    template = env.get_template(args.lang + ".j2")
+    m = Model(model_data)
 
     with args.output.open("w") as f:
-        f.write(
-            template.render(
-                disclaimer=f"This file was automatically generated by {THIS_FILE.name}. DO NOT MANUALLY MODIFY IT",
-                enums=template_enums,
-                classes=template_classes,
-            )
-        )
+        f.write(m.render_template(args.lang))
 
 
 if __name__ == "__main__":

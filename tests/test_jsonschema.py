70b8b33f8bd05a331f4aaaf2ca2a675f1bf54cc3
Joshua Watt <JPEWhacker@gmail.com>
2024-02-23 07:27:26 -0700

    jsonschema: Add schema tests
    Add tests that validate that the generated schema validates input
    properly

--- tests/test_jsonschema.py
+++ tests/test_jsonschema.py
@@ -6,133 +6,163 @@
 import re
 import subprocess
 import json
+import jsonschema
 import pytest
 from pathlib import Path
 
 THIS_FILE = Path(__file__)
 THIS_DIR = THIS_FILE.parent
 
-SPDX3_MODEL = THIS_DIR / "data" / "spdx3.jsonld"
+DATA_DIR = THIS_DIR / "data"
+
+SPDX3_MODEL = DATA_DIR / "spdx3.jsonld"
 SPDX3_EXPECT = THIS_DIR / "expect" / "jsonschema" / "spdx3.json"
 
-SPDX3_CONTEXT = THIS_DIR / "data" / "spdx3-context.json"
+SPDX3_CONTEXT = DATA_DIR / "spdx3-context.json"
 SPDX3_CONTEXT_URL = "https://spdx.github.io/spdx-3-model/context.json"
+SPDX3_CONTEXT_EXPECT = THIS_DIR / "expect" / "jsonschema" / "spdx3-context.json"
 
-pytestmark = pytest.mark.parametrize(
+SPDX3_CONTEXT_ARGS = ["--context-url", SPDX3_CONTEXT, SPDX3_CONTEXT_URL]
+
+
+@pytest.mark.parametrize(
     "expect,args",
     [
-        (
-            THIS_DIR / "expect" / "jsonschema" / "spdx3.json",
-            [],
-        ),
-        (
-            THIS_DIR / "expect" / "jsonschema" / "spdx3-context.json",
-            ["--context-url", SPDX3_CONTEXT, SPDX3_CONTEXT_URL],
-        ),
+        (SPDX3_EXPECT, []),
+        (SPDX3_CONTEXT_EXPECT, SPDX3_CONTEXT_ARGS),
     ],
 )
+class TestOutput:
+    def test_generation(self, tmpdir, expect, args):
+        """
+        Tests that shacl2code generates json schema output that matches the
+        expected output
+        """
+        outfile = tmpdir.join("spdx3.json")
+        subprocess.run(
+            [
+                "shacl2code",
+                "generate",
+                "--input",
+                SPDX3_MODEL,
+            ]
+            + args
+            + [
+                "jsonschema",
+                "--output",
+                outfile,
+            ],
+            check=True,
+        )
+
+        with expect.open("r") as f:
+            assert outfile.read() == f.read()
+
+    def test_output_syntax(self, expect, args):
+        """
+        Checks that the output file is valid json syntax by parsing it with Python
+        """
+        p = subprocess.run(
+            [
+                "shacl2code",
+                "generate",
+                "--input",
+                SPDX3_MODEL,
+            ]
+            + args
+            + [
+                "jsonschema",
+                "--output",
+                "-",
+            ],
+            check=True,
+            stdout=subprocess.PIPE,
+            encoding="utf-8",
+        )
+
+        json.loads(p.stdout)
+
+    def test_trailing_whitespace(self, expect, args):
+        """
+        Tests that the generated file does not have trailing whitespace
+        """
+        p = subprocess.run(
+            [
+                "shacl2code",
+                "generate",
+                "--input",
+                SPDX3_MODEL,
+            ]
+            + args
+            + [
+                "jsonschema",
+                "--output",
+                "-",
+            ],
+            check=True,
+            stdout=subprocess.PIPE,
+            encoding="utf-8",
+        )
+
+        for num, line in enumerate(p.stdout.splitlines()):
+            assert (
+                re.search(r"\s+$", line) is None
+            ), f"Line {num + 1} has trailing whitespace"
+
+    def test_tabs(self, expect, args):
+        """
+        Tests that the output file doesn't contain tabs
+        """
+        p = subprocess.run(
+            [
+                "shacl2code",
+                "generate",
+                "--input",
+                SPDX3_MODEL,
+            ]
+            + args
+            + [
+                "jsonschema",
+                "--output",
+                "-",
+            ],
+            check=True,
+            stdout=subprocess.PIPE,
+            encoding="utf-8",
+        )
+
+        for num, line in enumerate(p.stdout.splitlines()):
+            assert "\t" not in line, f"Line {num + 1} has tabs"
 
 
-def test_generation(tmpdir, expect, args):
-    """
-    Tests that shacl2code generates json schema output that matches the
-    expected output
-    """
-    outfile = tmpdir.join("spdx3.json")
-    subprocess.run(
-        [
-            "shacl2code",
-            "generate",
-            "--input",
-            SPDX3_MODEL,
-        ]
-        + args
-        + [
-            "jsonschema",
-            "--output",
-            outfile,
-        ],
-        check=True,
-    )
+@pytest.mark.parametrize(
+    "passes,file",
+    [
+        (True, "graph.json"),
+        (True, "graph-empty.json"),
+        (False, "graph-missing-context.json"),
+        (False, "graph-unknown-root-field.json"),
+        (False, "graph-unknown-field.json"),
+        (False, "graph-unknown-field-nested.json"),
+        (False, "graph-bad-context.json"),
+        (True, "root.json"),
+        (False, "root-missing-context.json"),
+        (False, "root-bad-context.json"),
+        (False, "root-unknown-field.json"),
+        (False, "root-unknown-field-nested.json"),
+        (False, "root-unknown-type.json"),
+        (False, "context-only.json"),
+    ],
+)
+def test_schema_validation(passes, file):
+    with SPDX3_CONTEXT_EXPECT.open("r") as f:
+        schema = json.load(f)
 
-    with expect.open("r") as f:
-        assert outfile.read() == f.read()
+    with (DATA_DIR / "jsonschema" / file).open("r") as f:
+        data = json.load(f)
 
-
-def test_output_syntax(expect, args):
-    """
-    Checks that the output file is valid json syntax by parsing it with Python
-    """
-    p = subprocess.run(
-        [
-            "shacl2code",
-            "generate",
-            "--input",
-            SPDX3_MODEL,
-        ]
-        + args
-        + [
-            "jsonschema",
-            "--output",
-            "-",
-        ],
-        check=True,
-        stdout=subprocess.PIPE,
-        encoding="utf-8",
-    )
-
-    json.loads(p.stdout)
-
-
-def test_trailing_whitespace(expect, args):
-    """
-    Tests that the generated file does not have trailing whitespace
-    """
-    p = subprocess.run(
-        [
-            "shacl2code",
-            "generate",
-            "--input",
-            SPDX3_MODEL,
-        ]
-        + args
-        + [
-            "jsonschema",
-            "--output",
-            "-",
-        ],
-        check=True,
-        stdout=subprocess.PIPE,
-        encoding="utf-8",
-    )
-
-    for num, line in enumerate(p.stdout.splitlines()):
-        assert (
-            re.search(r"\s+$", line) is None
-        ), f"Line {num + 1} has trailing whitespace"
-
-
-def test_tabs(expect, args):
-    """
-    Tests that the output file doesn't contain tabs
-    """
-    p = subprocess.run(
-        [
-            "shacl2code",
-            "generate",
-            "--input",
-            SPDX3_MODEL,
-        ]
-        + args
-        + [
-            "jsonschema",
-            "--output",
-            "-",
-        ],
-        check=True,
-        stdout=subprocess.PIPE,
-        encoding="utf-8",
-    )
-
-    for num, line in enumerate(p.stdout.splitlines()):
-        assert "\t" not in line, f"Line {num + 1} has tabs"
+    if passes:
+        jsonschema.validate(data, schema=schema)
+    else:
+        with pytest.raises(jsonschema.ValidationError):
+            jsonschema.validate(data, schema=schema)

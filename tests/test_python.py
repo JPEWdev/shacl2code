d803b056e000ffae7b3bfebe08ba985c6f59e2a1
Joshua Watt <JPEWhacker@gmail.com>
2025-11-05 15:09:36 -0700

    tests: Mark pyrefly tests as failing
    Newer versions of pyrefly appear to be overly pedantic and ignore the
    type annotations of functions, preferring to inspect the code for the
    actual return types. This breaks rdflib since it frequently has
    different type annotations from what functions might actually return
    (for example, `rdflib.terms.URIRef`). Since we can't think of a good
    work around for this, mark the tests as expected to fail for now.

--- tests/test_python.py
+++ tests/test_python.py
@@ -154,6 +154,7 @@ class TestCheckType:
             check=True,
         )
 
+    @pytest.mark.xfail(reason="pyrefly is ignoring type annotations from rdflib")
     def test_pyrefly(self, tmp_path, args):
         """
         Pyrefly static type checking

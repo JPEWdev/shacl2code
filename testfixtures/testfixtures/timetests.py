2ef665d1718b53d2084a90a0f17b655f93822bbc
Joshua Watt <JPEWhacker@gmail.com>
2025-12-17 11:20:13 -0700

    tests: Remove custom parametrize wrapper
    The custom parametrize wrapper was implemented to add ids to tests by
    passing them to the ids argument of pytest.mark.parametrize. However,
    this is unnecessary because pytest allows parameters to be of the type
    pytest.param, which accepts an id property directly.

--- testfixtures/testfixtures/timetests.py
+++ testfixtures/testfixtures/timetests.py
@@ -3,36 +3,36 @@
 #
 # SPDX-License-Identifier: MIT
 
+import pytest
+from pytest import param
 from datetime import datetime, timezone, timedelta
-from .utils import parametrize
 
 
 def datetime_encode_tests():
-    return parametrize(
+    return pytest.mark.parametrize(
         "value,expect",
         [
-            (
-                "UTC Timezone",
+            param(
                 datetime(2024, 3, 11, 1, 2, 3, tzinfo=timezone.utc),
                 "2024-03-11T01:02:03Z",
+                id="UTC Timezone",
             ),
-            (
-                "+0h (UTC) Timezone",
+            param(
                 datetime(2024, 3, 11, 1, 2, 3, tzinfo=timezone(timedelta(hours=0))),
                 "2024-03-11T01:02:03Z",
+                id="+0h (UTC) Timezone",
             ),
-            (
-                "+3h Timezone",
+            param(
                 datetime(2024, 3, 11, 1, 2, 3, tzinfo=timezone(timedelta(hours=3))),
                 "2024-03-11T01:02:03+03:00",
+                id="+3h Timezone",
             ),
-            (
-                "-3h Timezone",
+            param(
                 datetime(2024, 3, 11, 1, 2, 3, tzinfo=timezone(timedelta(hours=-3))),
                 "2024-03-11T01:02:03-03:00",
+                id="-3h Timezone",
             ),
-            (
-                "+3:21 Timezone",
+            param(
                 datetime(
                     2024,
                     3,
@@ -43,14 +43,14 @@ def datetime_encode_tests():
                     tzinfo=timezone(timedelta(hours=3, minutes=21)),
                 ),
                 "2024-03-11T01:02:03+03:21",
+                id="+3:21 Timezone",
             ),
-            (
-                "Microseconds Ignored",
+            param(
                 datetime(2024, 3, 11, 1, 2, 3, 999, tzinfo=timezone.utc),
                 "2024-03-11T01:02:03Z",
+                id="Microseconds Ignored",
             ),
-            (
-                "Timezone Seconds Ignored (+03:21:31)",
+            param(
                 datetime(
                     2024,
                     3,
@@ -61,9 +61,9 @@ def datetime_encode_tests():
                     tzinfo=timezone(timedelta(hours=3, minutes=21, seconds=31)),
                 ),
                 "2024-03-11T01:02:03+03:21",
+                id="Timezone Seconds Ignored (+03:21:31)",
             ),
-            (
-                "Timezone Seconds Ignored (-03:21:31)",
+            param(
                 datetime(
                     2024,
                     3,
@@ -74,42 +74,42 @@ def datetime_encode_tests():
                     tzinfo=timezone(timedelta(hours=-3, minutes=-21, seconds=-31)),
                 ),
                 "2024-03-11T01:02:03-03:21",
+                id="Timezone Seconds Ignored (-03:21:31)",
             ),
         ],
     )
 
 
 def datetime_decode_tests():
-    return parametrize(
+    return pytest.mark.parametrize(
         "value,expect",
         [
-            (
-                "No timezone",
+            param(
                 "2024-03-11T01:02:03",
                 datetime(2024, 3, 11, 1, 2, 3).astimezone(),
+                id="No timezone",
             ),
-            (
-                "UTC Timezone 'Z'",
+            param(
                 "2024-03-11T01:02:03Z",
                 datetime(2024, 3, 11, 1, 2, 3, tzinfo=timezone.utc),
+                id="UTC Timezone 'Z'",
             ),
-            (
-                "UTC Timezone '+00:00'",
+            param(
                 "2024-03-11T01:02:03+00:00",
                 datetime(2024, 3, 11, 1, 2, 3, tzinfo=timezone.utc),
+                id="UTC Timezone '+00:00'",
             ),
-            (
-                "+03:00 Timezone",
+            param(
                 "2024-03-11T01:02:03+03:00",
                 datetime(2024, 3, 11, 1, 2, 3, tzinfo=timezone(timedelta(hours=3))),
+                id="+03:00 Timezone",
             ),
-            (
-                "-03:00 Timezone",
+            param(
                 "2024-03-11T01:02:03-03:00",
                 datetime(2024, 3, 11, 1, 2, 3, tzinfo=timezone(timedelta(hours=-3))),
+                id="-03:00 Timezone",
             ),
-            (
-                "+03:21 Timezone",
+            param(
                 "2024-03-11T01:02:03+03:21",
                 datetime(
                     2024,
@@ -120,47 +120,47 @@ def datetime_decode_tests():
                     3,
                     tzinfo=timezone(timedelta(hours=3, minutes=21)),
                 ),
+                id="+03:21 Timezone",
             ),
-            (
-                "Microseconds not allowed",
+            param(
                 "2024-03-11T01:02:03.999Z",
                 None,
+                id="Microseconds not allowed",
             ),
-            (
-                "Timezone with seconds not allowed",
+            param(
                 "2024-03-11T01:02:03+03:00:01",
                 None,
+                id="Timezone with seconds not allowed",
             ),
         ],
     )
 
 
 def datetimestamp_decode_tests():
-    return parametrize(
+    return pytest.mark.parametrize(
         "value,expect",
         [
-            (
-                "UTC Timezone 'Z'",
+            param(
                 "2024-03-11T01:02:03Z",
                 datetime(2024, 3, 11, 1, 2, 3, tzinfo=timezone.utc),
+                id="UTC Timezone 'Z'",
             ),
-            (
-                "UTC Timezone '+00:00'",
+            param(
                 "2024-03-11T01:02:03+00:00",
                 datetime(2024, 3, 11, 1, 2, 3, tzinfo=timezone.utc),
+                id="UTC Timezone '+00:00'",
             ),
-            (
-                "+03:00 Timezone",
+            param(
                 "2024-03-11T01:02:03+03:00",
                 datetime(2024, 3, 11, 1, 2, 3, tzinfo=timezone(timedelta(hours=3))),
+                id="+03:00 Timezone",
             ),
-            (
-                "-03:00 Timezone",
+            param(
                 "2024-03-11T01:02:03-03:00",
                 datetime(2024, 3, 11, 1, 2, 3, tzinfo=timezone(timedelta(hours=-3))),
+                id="-03:00 Timezone",
             ),
-            (
-                "+03:21 Timezone",
+            param(
                 "2024-03-11T01:02:03+03:21",
                 datetime(
                     2024,
@@ -171,23 +171,24 @@ def datetimestamp_decode_tests():
                     3,
                     tzinfo=timezone(timedelta(hours=3, minutes=21)),
                 ),
+                id="+03:21 Timezone",
             ),
-            (
-                "No Timezone",
+            param(
                 "2024-03-11T01:02:03",
                 None,
+                id="No Timezone",
             ),
             # Microseconds not allowed
-            (
-                "Microseconds not allowed",
+            param(
                 "2024-03-11T01:02:03.999Z",
                 None,
+                id="Microseconds not allowed",
             ),
             # Timezone with seconds not allowed
-            (
-                "Timezone with seconds not allowed",
+            param(
                 "2024-03-11T01:02:03+03:00:01",
                 None,
+                id="Timezone with seconds not allowed",
             ),
         ],
     )

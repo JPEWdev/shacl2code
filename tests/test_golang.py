b382baeff46b11ddd79f80b3cdea6354efef3f6c
Arthit Suriyawongkul <arthit@gmail.com>
2026-07-04 21:07:31 +0100

    Remove stale -ra from pytest flag
    Signed-off-by: Arthit Suriyawongkul <arthit@gmail.com>

--- tests/test_golang.py
+++ tests/test_golang.py
@@ -1,6 +1,10 @@
 #
 # Copyright (c) 2024 Joshua Watt
 #
+# SPDX-FileContributor: Joshua Watt
+# SPDX-FileContributor: Arthit Suriyawongkul
+# SPDX-FileCopyrightText: 2024 Joshua Watt
+# SPDX-FileType: SOURCE
 # SPDX-License-Identifier: MIT
 
 import json
